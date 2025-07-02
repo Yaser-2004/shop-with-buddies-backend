8f3df23c89c7f198763d1a14c7a9fdb26baa6764
Yaser Siddiquee <yaser.siddiquee@gmail.com>
2025-07-02 10:26:41 +0530

    new commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,3 +9,17 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
+
+export const getProductById = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllProducts } from '../controllers/productController.js';
+import { getAllProducts, getProductById } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.get('/', getAllProducts);  // GET /api/products
+router.get('/:id', getProductById);
 
 export default router;
